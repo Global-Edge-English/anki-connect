0ecb8b5dec489bc5672e88b369c7aba0a7e36b25
Vasundhara Mehta <notestovasundhara@gmail.com>
2025-12-05 16:06:51 +0530

    add cards per day setting in deck stats

--- AnkiConnect.py
+++ AnkiConnect.py
@@ -67,7 +67,7 @@ except ImportError:
 #
 
 API_VERSION = 5
-ADDON_VERSION = "0.0.8"  # This will be auto-updated by build_zip.sh
+ADDON_VERSION = "0.0.9"  # This will be auto-updated by build_zip.sh
 TICK_INTERVAL = 25
 URL_TIMEOUT = 10
 URL_UPGRADE = 'https://raw.githubusercontent.com/FooSoft/anki-connect/master/AnkiConnect.py'
